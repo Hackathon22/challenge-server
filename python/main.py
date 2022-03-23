b9e57a97656a7675ca112c05c037b425ebce1f8d
Darius Couchard <darius.couchard@hotmail.com>
2022-03-23 20:44:43 +0100

    Added interpolation frames, but there is a problem with AI timings

--- python/main.py
+++ python/main.py
@@ -8,7 +8,10 @@ def my_ai(gamestate: SnapshotData, my_data: typing.Dict) -> Command:
 	else:
 		my_data['counter'] += 1
 
-	return MoveCommand((1.0, 1.0, 0.0))
+	if my_data['counter'] < 20:
+		return MoveCommand((0.0, -1.0, 0.0))
+	else:
+		return ShootCommand(0.0)
 
 if __name__ == '__main__':
 	simulation_list = []
